a87dd75f74a25768b4b32b1df48aafdcd9d602dd
Carlos Ocaña López <c.o.99@hotmail.com>
2024-03-02 10:51:50 +0100

    carrusel imagenes
    imagenes carrusel instalaciones

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function(){
     var header = this.document.querySelector("header")
     header.classList.toggle("abajo", this.window.scrollY>0)
 })
+
+document.addEventListener('DOMContentLoaded', function(){
+    // Selecciona el contenedor del carrusel y aplica la función slick()
+    $('.carrusel-imagenes').slick();
+});
